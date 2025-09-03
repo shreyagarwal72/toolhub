d07cab8001bd7c330632ab5e6589857250f82923
shreyagarwal72 <sanjayvansu1973@gmail.com>
2025-09-03 17:29:56 +0530

    Create server-worker.js

---
+++ server-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'nextuptools-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/logo/3.png'
+  // Add your tool JS/HTML files as needed
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
